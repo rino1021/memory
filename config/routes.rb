68859e5c3d52440daaeeebba63bb0cba9dc6e0ce
EC2 Default User <krat1021007@gmail.com>
2022-09-08 09:03:29 +0000

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'top' => 'homes#top'
+post 'photos' => 'photos#create'
 resources :photos ,only: [:new, :index, :show, :edit, :update, :destroy]
 end
 
