400ee3e4211bb197d1f2b924a0eb32805e27a910
EC2 Default User <krat1021007@gmail.com>
2022-09-09 06:12:06 +0000

    編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'top' => 'homes#top'
 get 'photos/new'
-get 'photos/edit'
 post 'photos' => 'photos#create'
 get 'photos' => 'photos#index'
 get 'photos/:id' => 'photos#show', as: 'photo'
+get 'photos/:id/edit' =>'photos#edit', as: 'edit_photo'
+patch 'photo/:id' => 'photos#update', as: 'update_photo'
 resources :photos ,only: [:new, :index, :show, :edit, :update, :destroy]
 end
 
