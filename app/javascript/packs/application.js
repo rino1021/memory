fa3f77fc7e853288a57de4b06c06958d77039df6
EC2 Default User <krat1021007@gmail.com>
2022-09-14 10:54:56 +0000

    カルーセル機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,17 @@ import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 import "test.js"
 
+// スライドショー
+// Turbolinks無効化(詳しくは過去記事参照)
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    // 自動再生
+    autoplay:true,
+    // ドットの表示
+    dots:true
+  });
+});
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
